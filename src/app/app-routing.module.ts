82f3e4a7799b72892a792d3e42451cc8ad005bd8
Liliia Kobzeva <liliia.taran@dev.pro>
2023-08-04 11:44:40 +0700

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'notes/0', pathMatch: 'full' },
-  { path: 'notes/:id', component: AppComponent },
+  { path: '', redirectTo: 'notes-app/notes/0', pathMatch: 'full' },
+  { path: 'notes-app/notes/:id', component: AppComponent },
 ];
 
 @NgModule({
